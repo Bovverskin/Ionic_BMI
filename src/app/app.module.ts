c6eff53e58a3176525a76d0a4dbf947f10512302
Jonathan <jon1969edwards@gmail.com>
2018-12-10 16:05:30 +0100

    Adds calculator tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CalculatorPage } from '../pages/calculator/calculator';
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
+    CalculatorPage,
     TabsPage
   ],
   imports: [
@@ -23,6 +25,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
+    CalculatorPage,
     TabsPage
   ],
   providers: [
