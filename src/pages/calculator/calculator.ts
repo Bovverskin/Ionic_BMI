1b94ec31131e10e4fd14150db6f245397f7d43d4
Jonathan <jon1969edwards@gmail.com>
2018-12-15 15:00:44 +0100

    Creates e2e test file and calculator.spec

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, } from 'ionic-angular';
-
-
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { empty } from 'rxjs/Observer';
 
 @IonicPage()
 @Component({
@@ -12,35 +11,62 @@ export class CalculatorPage {
   height: number;
   weight: number;
   bmiValue: number;
-  bmiMessage: string;
+  bmiMessage: any;
+  weightOption: string;
+  heightOption: string;
+  method: string;
+  switcher: boolean;
 
-  constructor() {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.weightOption = 'kg';
+    this.heightOption= 'cm';
+    this.method = 'Metric';
+    this.switcher = false;
   }
 
-  calculateBMI() {
-    if (this.weight > 0 && this.height > 0) {
-      let finalBmi = this.weight / (this.height / 100 * this.height / 100);
-      this.bmiValue = parseFloat(finalBmi.toFixed(2));
+  calculateBMI(){
+    this.switcher = true
+    if( this.weight > 0 && this.height > 0 ) {
+      let calculation;
+      if(this.method == "Metric"){
+        calculation = this.weight / ( this.height / 100 * this.height / 100 );
+      }else if (this.method == "Imperial") {
+        calculation = this.weight * 703 / ( this.height * this.height );
+      };
+      this.bmiValue = parseFloat( calculation.toFixed( 2 ) );
       this.setBMIMessage();
     }
   }
 
-
   private setBMIMessage() {
-    if (this.bmiValue < 18.5) {
-      this.bmiMessage = "Underweight"
+    if ( this.bmiValue < 18.5 ) {
+      this.bmiMessage = "underweight";
     }
-
-    if (this.bmiValue > 18.5 && this.bmiValue < 25) {
-      this.bmiMessage = "Normal"
+    else if ( this.bmiValue > 18.5 && this.bmiValue < 25 ) {
+      this.bmiMessage = "normal weight";
     }
-
-    if (this.bmiValue > 25 && this.bmiValue < 30) {
-      this.bmiMessage = "Overweight"
+    else if ( this.bmiValue > 25 && this.bmiValue < 30) {
+      this.bmiMessage = "overweight";
     }
+    else if ( this.bmiValue > 30 ) {
+      this.bmiMessage = "obese";
+    }
+  }
 
-    if (this.bmiValue > 30) {
-      this.bmiMessage = "Obese"
+  switchCounting() {
+    this.switcher = false
+    if( this.method == 'Metric' ) {
+      this.weightOption = 'pounds';
+      this.heightOption = 'inches';
+      this.method = 'Imperial';
+      this.weight = 0;
+      this.height= 0;
+    }else{
+      this.weightOption = 'kgs';
+      this.heightOption = 'cms';
+      this.method = 'Metric';
+      this.weight = 0;
+      this.height= 0;
     }
   }
 }
